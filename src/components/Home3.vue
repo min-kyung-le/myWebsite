4d223f73b2975cfa040574b911cba60fb2cad942
mink <minkyung1435@gmail.com>
2023-02-17 23:32:21 +0900

    메뉴 애니메이션 추가

--- src/components/Home3.vue
+++ src/components/Home3.vue
@@ -1,5 +1,5 @@
 <template>
-	<div ref="target" class="box-snaps home-3">
+	<div ref="target" id="home3" class="box-snaps home-3">
 		<div
 			href="name-box"
 			:id="['list' + (idx + 1)]"
@@ -34,7 +34,7 @@
 					</clipPath>
 				</defs>
 			</svg>
-			<div class="name-box">{{ m.name }}</div>
+			<div :class="['name-box name-box' + (idx + 1)]">{{ m.name }}</div>
 		</div>
 	</div>
 </template>
@@ -42,6 +42,8 @@
 <script>
 let text = [{ name: 'Forma Brands' }, { name: 'Function Health' }, { name: 'Calexo' }, { name: 'HP Omen' }]
 
+import { gsap } from 'gsap'
+let tl = gsap.timeline()
 export default {
 	name: 'Home3',
 	data() {
@@ -51,7 +53,30 @@ export default {
 		}
 	},
 	mounted() {
-		this.setWidthBox()
+		let thisId = ''
+		document.getElementById('home3').addEventListener('mouseover', e => {
+			thisId = e.target.id
+
+			if (thisId.indexOf('list') != -1) {
+				tl.to('.name-box' + thisId.charAt(thisId.length - 1), {
+					x: 100,
+					duration: 0.2,
+					ease: 'power1.inOut',
+				})
+			}
+		})
+		document.getElementById('home3').addEventListener('mouseout', e => {
+			thisId = e.target.id
+			console.log(thisId)
+
+			if (thisId.indexOf('list') != -1) {
+				tl.to('.name-box' + thisId.charAt(thisId.length - 1), {
+					x: 0,
+					duration: 0.2,
+					ease: 'power1.inOut',
+				})
+			}
+		})
 	},
 	methods: {
 		setWidthBox() {
@@ -59,7 +84,7 @@ export default {
 			let setWidth = window.innerWidth - 160
 			let boxs = document.getElementsByClassName('name-box')
 			for (let box of boxs) {
-				box.style.width = (setWidth / 5) * 3 + 'px'
+				box.style.width = setWidth + 'px'
 			}
 		},
 	},
@@ -70,7 +95,8 @@ export default {
 .list {
 	font-size: 3.2rem;
 	color: #f1f1f1;
-	background-color: #242424;
+	/* background-color: #242424; */
+	background-color: yellow;
 	padding: 1px 4px 2px 4px;
 	height: 80px;
 	margin-bottom: 10px;
@@ -79,17 +105,25 @@ export default {
 .name-box {
 	display: inline-block;
 	position: absolute;
+	left: 98px;
+	padding-top: 28px;
+	height: 80px;
+}
+
+.name-box-old {
+	display: inline-block;
+	position: absolute;
 	left: 70px;
 	padding-top: 28px;
 	height: 80px;
 }
-.name-box:hover {
+.name-box-old:hover {
 	animation-name: boxmove;
 	animation-duration: 500ms;
 	animation-fill-mode: forwards;
 }
 
-.name-box:before {
+.name-box-old:before {
 	animation-name: boxmoveback;
 	animation-duration: 500ms;
 	animation-fill-mode: forwards;
