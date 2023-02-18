a0c9195d3cde59e9cebbc5780180471cbd2f453d
mink <minkyung1435@gmail.com>
2023-02-18 13:07:19 +0900

    홈3 글씨에 올려도 이벤트 되도록

--- src/components/Home3.vue
+++ src/components/Home3.vue
@@ -2,13 +2,14 @@
 	<div ref="target" id="home3" class="box-snaps home-3">
 		<div :id="['list' + (idx + 1)]" :class="['list list' + (idx + 1)]" v-for="(m, idx) of menu" :key="m">
 			<svg
-				width="15%"
+				:class="['arrow-right-black-svg' + (idx + 1)]"
+				width="0"
 				height="100%"
-				viewBox="155 -10 94 60"
+				viewBox="0 -10 94 60"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
 				style="opacity: 1">
-				<g clip-path="url(#clip0_651_11245)" :class="['arrow-right-black-svg' + (idx + 1)]">
+				<g clip-path="url(#clip0_651_11245)">
 					<path
 						d="M0.5 29.5H94L81 17"
 						class="arrow-right-black"
@@ -28,7 +29,7 @@
 					</clipPath>
 				</defs>
 			</svg>
-			<div :class="['name-box name-box' + (idx + 1)]">{{ m.name }}</div>
+			{{ m.name }}
 		</div>
 	</div>
 </template>
@@ -38,7 +39,6 @@ let text = [{ name: 'Forma Brands' }, { name: 'Function Health' }, { name: 'Cale
 
 import { gsap } from 'gsap'
 let tl = gsap.timeline()
-let moveDelay
 let moveOver
 export default {
 	name: 'Home3',
@@ -49,52 +49,40 @@ export default {
 		}
 	},
 	mounted() {
-		let thisId = ''
-		moveDelay = 1.5
-		document.getElementById('home3').addEventListener('mouseover', e => {
-			thisId = e.target.id
-			if (thisId.indexOf('list') != -1) {
-				tl.to(
-					[
-						'.name-box' + thisId.charAt(thisId.length - 1),
-						'.arrow-right-black-svg' + thisId.charAt(thisId.length - 1),
-					],
-					{
-						x: 125,
+		this.menuEffect()
+	},
+	methods: {
+		menuEffect() {
+			let thisId = ''
+			document.getElementById('home3').addEventListener('mouseover', e => {
+				thisId = e.target.id
+				if (thisId.indexOf('list') != -1) {
+					tl.to('.arrow-right-black-svg' + thisId.charAt(thisId.length - 1), {
+						width: '125px',
 						duration: 0.2,
 						ease: 'power3.easeInOut',
-					}
-				)
-				moveDelay = 0
-				moveOver = 1
-			}
-		})
-		document.getElementById('home3').addEventListener('mouseout', e => {
-			thisId = e.target.id
+					})
+					moveOver = 1
+				}
+			})
+			document.getElementById('home3').addEventListener('mouseout', e => {
+				thisId = e.target.id
 
-			if (thisId.indexOf('list') != -1) {
-				tl.to(
-					[
-						'.name-box' + thisId.charAt(thisId.length - 1),
-						'.arrow-right-black-svg' + thisId.charAt(thisId.length - 1),
-					],
-					{
-						x: 0,
+				if (thisId.indexOf('list') != -1) {
+					tl.to('.arrow-right-black-svg' + thisId.charAt(thisId.length - 1), {
+						width: '0px',
 						duration: 0.2,
 						ease: 'power2.easeInOut',
 						overwrite: true,
 						onComplete: () => {
 							if (moveOver === 1) {
-								moveDelay = 1.5
-								moveOver = null
+								moveOver = 0
 							}
 						},
-					}
-				)
-			}
-		})
-	},
-	methods: {
+					})
+				}
+			})
+		},
 		setWidthBox() {
 			// padding-left 80, padding-right 80
 			let setWidth = window.innerWidth - 160
@@ -112,17 +100,9 @@ export default {
 	font-size: 3.2rem;
 	color: #f1f1f1;
 	background-color: #242424;
-	padding: 1px 4px 2px 4px;
+	padding: 8px 4px 5px 4px;
 	height: 80px;
-	margin-bottom: 10px;
-}
-
-.name-box {
-	display: inline-block;
-	position: absolute;
-	left: 98px;
-	padding-top: 28px;
-	height: 80px;
+	border-bottom: solid 1px #f1f1f1;
 }
 
 .name-box-old {
