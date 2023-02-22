cf002dbafe961a15cda8f474a9fc654dbc66fe61
mink <minkyung1435@gmail.com>
2023-02-22 15:24:01 +0900

    홈2 수정

--- src/components/Home2.vue
+++ src/components/Home2.vue
@@ -2,16 +2,18 @@
 	<div ref="target" class="box-snaps holster-in-box">
 		<div class="a"></div>
 		<div class="secense-wrap" :style="setStyle">
-			<div class="secense secense-1"></div>
-			<div class="secense secense-2"></div>
-			<div class="secense secense-3"></div>
-			<div class="secense secense-4"></div>
-			<div class="secense secense-5"></div>
+			<div class="title">자신있게 말씀드리는 핵심 역량</div>
+			<div class="secense secense-1">
+				고객사에 파견되어 근무를 했기 때문에 다양한 사람들과 원활한 소통이 가능합니다.
+			</div>
+			<div class="secense secense-2">Javascript, Vue, Vuetify, chart.js로 프론트엔드 웹 개발을 진행했습니다.</div>
+			<div class="secense secense-3">JAVA, JPA, Database, Node.js를 이용한 자사솔루션 개발을 진행했습니다.</div>
+			<div class="secense secense-4">망설임없이 도전하는 성격으로 최신 IT경향, 웹트렌드에 민감합니다.</div>
+			<div class="title front-title"></div>
 			<div class="front front-1"></div>
 			<div class="front front-2"></div>
 			<div class="front front-3"></div>
 			<div class="front front-4"></div>
-			<div class="front front-5"></div>
 			<div class="secense-done"></div>
 		</div>
 	</div>
@@ -31,8 +33,8 @@ export default {
 	name: 'Home2',
 	data() {
 		return {
+			title: '자신있게 말씀드리는 핵심 역량',
 			textarea: [
-				'자신있게 말씀드리는 핵심 역량',
 				'고객사에 파견되어 근무를 했기 때문에 다양한 사람들과 원활한 소통이 가능합니다.',
 				'Javascript, Vue, Vuetify, chart.js로 프론트엔드 웹 개발을 진행했습니다.',
 				'JAVA, JPA, Database, Node.js를 이용한 자사솔루션 개발을 진행했습니다.',
@@ -46,13 +48,12 @@ export default {
 		},
 	},
 	mounted() {
-		this.setTextarea()
 		let secense = document.getElementsByClassName('secense')
 		let fronts = document.getElementsByClassName('front')
 
 		let i = 0
 		for (let sec of secense) {
-			let setclientHeight = sec.clientHeight * i
+			let setclientHeight = sec.clientHeight * i + 110
 			fronts[i].style.top = setclientHeight + 'px'
 			i++
 		}
@@ -61,19 +62,23 @@ export default {
 		this.isTypeing()
 	},
 	methods: {
-		setTextarea() {
-			gsap.utils.toArray('.secense').forEach((front, index) => {
-				tl.set(front, {
-					text: {
-						value: this.textarea[index],
-						delimiter: '',
-					},
-				})
-			})
-		},
 		isTypeing() {
 			let rough =
 				'rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })'
+
+			tl.to('.front-title', {
+				scrollTrigger: {
+					trigger: '.front-title',
+					start: 'top-=200 center',
+					end: 'bottom-=130 center',
+					scrub: 2,
+				},
+				text: {
+					value: this.title,
+					delimiter: '',
+				},
+				ease: rough,
+			})
 
 			gsap.utils.toArray('.front').forEach((front, index) => {
 				tl.to(front, {
